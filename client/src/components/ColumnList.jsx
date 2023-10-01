98f0c7f99ae06bd5260ed1b1f22212392bda81ad
caro-turnbull <caro.turnbull@gmail.com>
2023-10-01 15:16:58 -0400

    fix bug

--- client/src/components/ColumnList.jsx
+++ client/src/components/ColumnList.jsx
@@ -3,16 +3,24 @@ import ColumnListItem from "./ColumnListItem";
 import "../styles/ColumnList.scss";
 import { DragDropContext } from "react-beautiful-dnd";
 
+//mock data
+const columnData = [
+  { id: 1, name: "To Do" },
+  { id: 2, name: "In Progress" },
+  { id: 3, name: "In Review" },
+  { id: 4, name: "Complete!" }
+];
+
 
 const ColumnList = (props) => {
+
   return (
     <DragDropContext>
       <div>
         <h1>This is ColumnList</h1>
         <ul className="columnlist">
           <ColumnListItem />
-          {/* <ColumnListItem />
-          <ColumnListItem /> */}
+
         </ul>
       </div>
     </ DragDropContext>
