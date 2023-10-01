7f92309f86b5d9cfab1e3e7df3b80ec8d3764125
caro-turnbull <caro.turnbull@gmail.com>
2023-10-01 16:57:09 -0400

    four collumns

--- client/src/components/ColumnList.jsx
+++ client/src/components/ColumnList.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import ColumnListItem from "./ColumnListItem";
 import "../styles/ColumnList.scss";
 import { DragDropContext } from "react-beautiful-dnd";
@@ -11,15 +11,21 @@ const columnData = [
   { id: 4, name: "Complete!" }
 ];
 
+// const [tasks, setTasks] = useState();
+// function handleOnDragEnd(result) {
+// }
 
 const ColumnList = (props) => {
 
+  const columnArr = columnData.map((column, index) => <ColumnListItem key={column.id} name={column.name} />);
+
   return (
-    <DragDropContext>
+    <DragDropContext >
+      {/* <DragDropContext onDragEnd={handleOnDragEnd}> */}
       <div>
         <h1>This is ColumnList</h1>
         <ul className="columnlist">
-          <ColumnListItem />
+          {columnArr}
 
         </ul>
       </div>
--- client/src/components/ColumnListItem.jsx
+++ client/src/components/ColumnListItem.jsx
@@ -3,13 +3,11 @@ import TaskList from "./TaskList";
 import "../styles/ColumnListItem.scss";
 
 const ColumnListItem = (props) => {
-
+  const { name } = props;
   return (
 
-
-
     <div>
-      <h2>This is ColumListItem - To Do</h2>
+      <h2>This is ColumListItem {name}</h2>
 
 
       <TaskList />
