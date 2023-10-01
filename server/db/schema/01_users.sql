3682dc980adf3b2c8c737e331e0c2e420a23b4a1
caro-turnbull <caro.turnbull@gmail.com>
2023-10-01 08:43:14 -0400

    tasks mocks

--- server/db/schema/01_users.sql
+++ server/db/schema/01_users.sql
@@ -2,6 +2,7 @@ DROP TABLE IF EXISTS users CASCADE;
 -- CREATE USERS
 CREATE TABLE users (
   id SERIAL PRIMARY KEY,
+  username VARCHAR(50) NOT NULL,
   email VARCHAR(255) NOT NULL,
   password VARCHAR(255) NOT NULL
   -- avatar tbd-filelocation
--- server/db/seeds/01_users.sql
+++ server/db/seeds/01_users.sql
@@ -1,19 +1,22 @@
-INSERT INTO users (email, password) 
+INSERT INTO users (username, email, password) 
   VALUES 
-    ('rick.sandchez@gmail.com', 'picklerick'),
-    ('lisa.simpson@gmail.com', 'ehhhhh'),
-    ('link@yahoo.com', 'hyrule'),
-    ('simon_bel123@mail.ca', 'dracula'),
-    ('all_might@academia.jp', 'plusUltra'),
-    ('mario@mushroomkindom.jp', 'plumber79')
+    ('Caroline', 'caro@example.com', 'passwrd'),
+    ('Yuli', 'yuli@example.com', 'passwrd'),
+    ('Max', 'max@eaxmple.com', 'passwrd'),
+    ('Edwin', 'edwin@example.com', 'passwrd'),
+    ('Curtis', 'curtis@example.com', 'passwrd'),
+    ('Salman', 'salman@example.com', 'passwrd'),
+    ('Shani', 'shani@example.com', 'passwrd')
+
 ;
 
 -- -- as mock
--- const usersMockObj = {
---   {id: 1, email:'rick.sandchez@gmail.com', password: 'picklerick'},
---   {id: 2, email:'lisa.simpson@gmail.com', password: 'ehhhhh'},
---   {id: 3, email:'link@yahoo.com', password: 'hyrule'},
---   {id: 4, email:'simon_bel123@mail.ca', password: 'dracula'},
---   {id: 5, email:'all_might@academia.jp', password: 'plusUltra'},
---   {id: 6, email:'mario@mushroomkindom.jp', password: 'plumber79'}
--- }
+-- const usersMockArr = [
+--   {id: 1, name:'Caroline', email: 'caro@example.com', password: 'passwrd'},
+--   {id: 2, name:'Yuli', email: 'yuli@example.com, password:  'passwrd'},
+--   {id: 3, name:'Max', email: 'max@eaxmple.com', password: 'passwrd'},
+--   {id: 4, name:'Edwin', email: 'edwin@example.com', password: 'passwrd'},
+--   {id: 5, name:'Curtis', email: 'curtis@example.com', password: 'passwrd'},
+--   {id: 6, name:'Salman', email: 'salman@example.com', password: 'passwrd'},
+--   {id: 7, name:'Shani', email: 'shani@example.com', password: 'passwrd'}
+-- ]
