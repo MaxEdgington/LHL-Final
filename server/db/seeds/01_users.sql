3408f76839d931d01cf4598a7d0a0aa36f8e12c7
caro-turnbull <caro.turnbull@gmail.com>
2023-09-30 20:18:49 -0400

    schemas created, some mocks, avatar images

--- server/db/seeds/01_users.sql
+++ server/db/seeds/01_users.sql
@@ -1,6 +1,19 @@
-INSERT INTO users (id, email, password) VALUES (1, 'rick.sandchez@gmail.com', 'picklerick');
-INSERT INTO users (id, email, password) VALUES (2, 'lisa.simpson@gmail.com', 'ehhhhh');
-INSERT INTO users (id, email, password) VALUES (3, 'link@yahoo.com', 'hyrule');
-INSERT INTO users (id, email, password) VALUES (4, 'simon_bel123@mail.ca', 'dracula');
-INSERT INTO users (id, email, password) VALUES (5, 'all_might@academia.jp', 'plusUltra');
-INSERT INTO users (id, email, password) VALUES (6, 'mario@mushroomkindom.jp', 'plumber79');
+INSERT INTO users (email, password) 
+  VALUES 
+    ('rick.sandchez@gmail.com', 'picklerick'),
+    ('lisa.simpson@gmail.com', 'ehhhhh'),
+    ('link@yahoo.com', 'hyrule'),
+    ('simon_bel123@mail.ca', 'dracula'),
+    ('all_might@academia.jp', 'plusUltra'),
+    ('mario@mushroomkindom.jp', 'plumber79')
+;
+
+-- -- as mock
+-- const usersMockObj = {
+--   {id: 1, email:'rick.sandchez@gmail.com', password: 'picklerick'},
+--   {id: 2, email:'lisa.simpson@gmail.com', password: 'ehhhhh'},
+--   {id: 3, email:'link@yahoo.com', password: 'hyrule'},
+--   {id: 4, email:'simon_bel123@mail.ca', password: 'dracula'},
+--   {id: 5, email:'all_might@academia.jp', password: 'plusUltra'},
+--   {id: 6, email:'mario@mushroomkindom.jp', password: 'plumber79'}
+-- }
