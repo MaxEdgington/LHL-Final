ae824f793660d7a9145f429a1e0f6a266cb6852a
caro-turnbull <caro.turnbull@gmail.com>
2023-10-02 12:24:21 -0400

    MUI components added to UI, still need to be themed

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -13,7 +13,7 @@ import Tooltip from '@mui/material/Tooltip';
 import MenuItem from '@mui/material/MenuItem';
 import AdbIcon from '@mui/icons-material/Adb';
 
-const pages = ['New Project', 'TBA', 'TBA'];
+const pages = ['New Project', 'TBA', 'What Links?'];
 const settings = ['Profile', 'My Projects', 'Logout'];
 
 function Header() {
--- client/src/components/TaskListItem.jsx
+++ client/src/components/TaskListItem.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import "../styles/TaskListItem.scss";
+import { Box, Card, CardContent, CardActions, Typography, Avatar } from "@mui/material";
 import { Draggable } from "react-beautiful-dnd";
 
 
@@ -7,17 +8,43 @@ import { Draggable } from "react-beautiful-dnd";
 const TaskListItem = (props) => {
   const { id, name, index } = props;
 
-  // console.log("some ID", id);
-  // console.log("typeOF", typeof String(id));
+  const getItemStyle = (isDragging, draggableStyle) => ({
+    // some basic styles to make the items look a bit nicer
+    userSelect: "none",
+    // padding: grid * 2,
+    // margin: `0 0 ${grid}px 0`,
+
+    // change background colour if dragging
+    background: isDragging ? "#e53637" : "white",
+
+    // styles we need to apply on draggables
+    ...draggableStyle
+  });
 
   return (
     <Draggable key={id} draggableId={String(id)} index={index}>
-      {(provided) => (
-        <li ref={provided.innerRef} {...provided.draggableProps} {...provided.dragHandleProps}>
+      {(provided, snapshot) => (
 
-          {name}
+        <Box sx={{ marginBottom: 1 }}>
+          <Card>
+            <CardContent>
+              <Typography variant="h5" component="div">
 
-        </li>
+                <div ref={provided.innerRef} {...provided.draggableProps} {...provided.dragHandleProps}
+                  style={getItemStyle(snapshot.isDragging, provided.draggableProps.style)}>
+
+                  {name}
+                </div>
+
+              </Typography>
+            </CardContent>
+
+            <CardActions>
+              {/* somethign in here for clickable??? from mui? */}
+            </CardActions>
+
+          </Card>
+        </Box>
       )}
     </Draggable>
   );
