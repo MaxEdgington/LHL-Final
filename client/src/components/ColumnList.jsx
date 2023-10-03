f4f4211d4a6da61cc614ab95978f53cf9a7e6d59
Max Edgington <edgington.m.w@gmail.com>
2023-10-03 19:36:20 -0400

    branch init

--- client/src/components/ColumnList.jsx
+++ client/src/components/ColumnList.jsx
@@ -1,130 +1,21 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useContext } from "react";
 import ColumnListItem from "./ColumnListItem";
 import axios from "axios";
 import ChatDrawer from "./ChatDrawer";
 import { Box, Typography } from "@mui/material";
 import "../styles/ColumnList.scss";
 import { DragDropContext } from "react-beautiful-dnd";
-
-const taskMockArr = [
-  {
-    id: 1,
-    name: "Project Planning and Ideation",
-    description: "Define project objectives, create a timeline, and brainstorm unique features.",
-    project_id: 1,
-    due_date: "2023-10-12",
-    // status: 'To Do',
-    assigned_user: 1
-  },
-  {
-    id: 2,
-    name: "Market Research",
-    description: "Research pet and cat-related social media platforms for insights and opportunities.",
-    project_id: 1,
-    due_date: "2023-10-13",
-    // status: 'To Do',
-    assigned_user: 2
-  },
-  {
-    id: 3,
-    name: "Technology Stack Selection",
-    description: "Choose front-end and back-end technologies, including programming languages and databases.",
-    project_id: 1,
-    due_date: "2023-10-14",
-    // status: 'To Do',
-    assigned_user: 3
-  },
-  {
-    id: 4,
-    name: "UI/UX Design",
-    description: "Create wireframes, mockups, and a visually appealing cat-themed interface.",
-    project_id: 1,
-    due_date: "2023-10-13",
-    // status: 'To Do',
-    assigned_user: 1
-  },
-  {
-    id: 5,
-    name: "Front-End Development",
-    description: "Develop the front-end using HTML, CSS, and JavaScript with relevant frameworks.",
-    project_id: 1,
-    due_date: "2023-11-14",
-    // status: 'To Do',
-    assigned_user: 4
-  },
-  {
-    id: 6,
-    name: "Back-End Development",
-    description: "Build server-side logic, implement authentication, and create APIs for key features.",
-    project_id: 1,
-    due_date: "2023-11-15",
-    // status: 'To Do',
-    assigned_user: 6
-  },
-  {
-    id: 7,
-    name: "Database Setup and Management",
-    description: "Design the database schema, set up the database server, and implement queries.",
-    project_id: 1,
-    due_date: "2023-12-15",
-    // status: 'To Do',
-    assigned_user: 5
-  },
-  {
-    id: 8,
-    name: "Testing, Deployment, and Monitoring",
-    description: "Test for bugs, deploy to a web server, and set up monitoring for performance.",
-    project_id: 1,
-    due_date: "2023-12-17",
-    // status: 'To Do',
-    assigned_user: 1
-  }
-];
-
-const initialColumnData = {
-  1: { name: "To Do", tasks: [] },
-  2: { name: "In Progress", tasks: [] },
-  3: { name: "In Review", tasks: [] },
-  4: { name: "Complete!", tasks: [] }
-};
+import { columnsContext } from "../providers/ColumnsProvider" 
 
 const ColumnList = (props) => {
-
-  const [columns, setColumns] = useState(initialColumnData);
+  const { columns, fetchTasks } = useContext(columnsContext)
+ 
 
   
   // Start of new code 
 
   useEffect(() => {
-  const fetchTasks = async () => {
-    try {
-      const res = await axios.get("http://localhost:8080/api/tasks");
-      console.log("Tasks received from server:", res.data); 
-
-      // ... rest of your logging and fetching logic ...
-
-      // Creating a new column state based on the fetched tasks
-      // setColumns(prevColumns =>
-      //   Object.keys(prevColumns).reduce((acc, columnId) => {
-      //     acc[columnId] = {
-      //       ...prevColumns[columnId],
-      //       tasks: fetchTasks[columnId] || [],
-      //     };
-      //     return acc;
-      //   }, {})
-      // );
-
-      setColumns(prevColumns => {
-        // console.log("Mentor Session: Looking for prevColumns", prevColumns[0])
-        // prevColumns[0].tasks = res.data
-        let newTasks = {}
-        return {
-          ...prevColumns, 
-          [1]:{
-            ...prevColumns[1], 
-            tasks:res.data
-          }}
-      } )
+  
 
       console.log("After data transformation:", columns);
     } catch (error) {
