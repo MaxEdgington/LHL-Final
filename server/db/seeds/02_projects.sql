55b66674c9a5bbd0075749fe51ef5e631c3f9297
caro-turnbull <caro.turnbull@gmail.com>
2023-10-03 21:03:07 -0400

    updated db seeds and schema

--- server/db/seeds/02_projects.sql
+++ server/db/seeds/02_projects.sql
@@ -1,4 +1,7 @@
-
+INSERT INTO projects (name, description, due_date, owner_id)
+VALUES
+  ('Twitter for Cats', 'A twitter clone where cats can send Meowts to eachother.', '2023-12-12', 2),
+  ('LHL Finals', 'How am I going to make all of this in just a week', '2023-10-12', 1)
 
  -- -- as mock:
 -- const projectsMock = {
