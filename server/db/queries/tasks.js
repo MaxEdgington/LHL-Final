4158701c1a15bc8776e88d62e331b1e0f817f909
Yuli Wang <yuliwang1007@gmail.com>
2023-10-04 19:01:37 -0400

    hard coded project_id as 1 in queries/tasks.js

--- server/db/queries/tasks.js
+++ server/db/queries/tasks.js
@@ -5,8 +5,9 @@ const addNewTask = async (title, column_id) => {
     // As per our seed data, the tasks table have columns: name, description, due_date.
     // Here, we're only adding name (title from frontend) and column_id. We can expand this later.
     const result = await db.query(
-      "INSERT INTO tasks (name) VALUES ($1) RETURNING *",
-      [title]
+      "INSERT INTO tasks (name, project_id) VALUES ($1, $2) RETURNING *",
+      [title,1]
+      // We hard coded project_id as 1
     );
 
     return result.rows[0];
