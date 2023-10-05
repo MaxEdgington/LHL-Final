1deb53747fe2c6e8f9264b04073c056e93dca373
Max Edgington <edgington.m.w@gmail.com>
2023-10-05 00:43:24 -0400

    modify table and seed data to use task-status

--- server/db/seeds/04_tasks.sql
+++ server/db/seeds/04_tasks.sql
@@ -1,23 +1,23 @@
 
 -- with project ID and assigned_user
 
-INSERT INTO tasks (name, description, due_date, project_id, assigned_user)
+INSERT INTO tasks (name, description, due_date, status, project_id, assigned_user)
 VALUES
-  ('Project Planning and Ideation', 'Define project objectives, create a timeline, and brainstorm unique features.', '2023-10-12', 1, 1),
+  ('Project Planning and Ideation', 'Define project objectives, create a timeline, and brainstorm unique features.', '2023-10-12', '1', 1, 1),
 
-  ('Market Research', 'Research pet and cat-related social media platforms for insights and opportunities.', '2023-11-12', 1, 2),
+  ('Market Research', 'Research pet and cat-related social media platforms for insights and opportunities.', '2023-11-12', '1', 1, 2),
 
-  ('Technology Stack Selection', 'Choose front-end and back-end technologies, including programming languages and databases.', '2023-12-12', 1, 3),
+  ('Technology Stack Selection', 'Choose front-end and back-end technologies, including programming languages and databases.', '2023-12-12', '2', 1, 3),
 
-  ('UI/UX Design', 'Create wireframes, mockups, and a visually appealing cat-themed interface.', '2023-12-13', 1, 1),
+  ('UI/UX Design', 'Create wireframes, mockups, and a visually appealing cat-themed interface.', '2023-12-13', '2', 1, 1),
 
-  ('Front-End Development', 'Develop the front-end using HTML, CSS, and JavaScript with relevant frameworks.', '2023-12-14', 1, 4),
+  ('Front-End Development', 'Develop the front-end using HTML, CSS, and JavaScript with relevant frameworks.', '2023-12-14', '3', 1, 4),
 
-  ('Back-End Development', 'Build server-side logic, implement authentication, and create APIs for key features.', '2023-12-15', 1, 6),
+  ('Back-End Development', 'Build server-side logic, implement authentication, and create APIs for key features.', '2023-12-15', '3', 1, 6),
 
-  ('Database Setup and Management', 'Design the database schema, set up the database server, and implement queries.', '2023-12-16', 1, 5),
+  ('Database Setup and Management', 'Design the database schema, set up the database server, and implement queries.', '2023-12-16', '4', 1, 5),
   
-  ('Testing, Deployment, and Monitoring', 'Test for bugs, deploy to a web server, and set up monitoring for performance.', '2023-12-17', 1, 7);
+  ('Testing, Deployment, and Monitoring', 'Test for bugs, deploy to a web server, and set up monitoring for performance.', '2023-12-17', '4', 1, 7);
 
 
 -- -- as mock:
