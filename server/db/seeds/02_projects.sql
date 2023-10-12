4592640f50a2e9c5587fd8ac3005bcf8b1de302c
caro-turnbull <caro.turnbull@gmail.com>
2023-10-12 10:06:38 -0400

    fix after pulled main in the server

--- server/db/seeds/02_projects.sql
+++ server/db/seeds/02_projects.sql
@@ -1,23 +1,23 @@
 
 --DO NOT REORDER THESE, OTHER SEEDS DEPEND ON THIS ORDER
-INSERT INTO projects (name, description, due_date, owner_id)
+INSERT INTO projects (id, name, description, due_date, owner_id)
 VALUES
-  ('Twitter for Cats', 'A twitter clone where cats can send Meowts to eachother.', '2023-12-12', 2),
-  ('LHL Finals', 'How am I going to make all of this in just a week?', '2023-10-12', 1),
-  ('Vend!', 'Midterm buy and sell resource wall', '2023-09-18', 3),
-  ('Pawsitive Adoption', 'Dog and Cat Adoption Site', '2023-10-13', 5),
-  ('Git Hub Search', 'A way to search for repos on git hub', '2023-10-13', 7),
-  ('Yes, No, Toaster', 'An app for deciding what to put on your toast in the morning','2023-11-30', 3),
-  ('Owen Wilson Fan Club', 'Wow', '2024-03-09', 4),
-  ('Rock Paper Scissors Lizard Spock', 'A rock paper scissors game where you can add lots more choices', '2023-11-09', 1),
-  ('Recipe Swap', 'Where foodies to exchange, rate, and discuss their favorite recipes.', '2023-12-25', 3),
-  ('EcoConnect', 'For environmental enthusiasts to discuss eco-friendly practices and projects.', '2024-01-12', 5),
-  ('Penguin Social Club', 'Where penguins can waddle around the internet, share fish recipes, and slide into each others icebergs.', '2023-10-22', 4),
-  ('Code Buddy', 'A platform for developers to find coding partners, collaborate on projects, and share code snippets.', '2024-01-31', 6),
-  ('TechGuru Chatbot', 'A chatbot that answers tech-related questions and provides information about the latest gadgets and technology trends.', '2023-11-29', 7),
-  ('ShopSaver', 'Helps users find the best deals, compare prices, and create shopping lists for a more budget-friendly shopping experience.', '2023-12-01', 2),
-  ('FitFusion', 'Offers a variety of workout routines, tracks fitness progress, and provides nutritional tips to help users achieve their fitness goals.', '2024-02-20', 3),
-  ('KidzCraft', 'An online space where children can explore and share their creative projects, including drawings, crafts, and stories.', '2023-03-03', 1);
+  (1, 'Twitter for Cats', 'A twitter clone where cats can send Meowts to eachother.', '2023-12-12', 2),
+  (2, 'LHL Finals', 'How am I going to make all of this in just a week?', '2023-10-12', 1),
+  (3, 'Vend!', 'Midterm buy and sell resource wall', '2023-09-18', 3),
+  (4, 'Pawsitive Adoption', 'Dog and Cat Adoption Site', '2023-10-13', 5),
+  (5, 'The Digital Architects', 'A way to search for repos on git hub', '2023-10-13', 7),
+  (6, 'Yes, No, Toaster', 'An app for deciding what to put on your toast in the morning','2023-11-30', 3),
+  (7, 'Owen Wilson Fan Club', 'Wow', '2024-03-09', 4),
+  (8, 'Rock Paper Scissors Lizard Spock', 'A rock paper scissors game where you can add lots more choices', '2023-11-09', 1),
+  (9, 'Recipe Swap', 'Where foodies to exchange, rate, and discuss their favorite recipes.', '2023-12-25', 3),
+  (10, 'EcoConnect', 'For environmental enthusiasts to discuss eco-friendly practices and projects.', '2024-01-12', 5),
+  (11, 'Penguin Social Club', 'Where penguins can waddle around the internet, share fish recipes, and slide into each others icebergs.', '2023-10-22', 4),
+  (12, 'Code Buddy', 'A platform for developers to find coding partners, collaborate on projects, and share code snippets.', '2024-01-31', 6),
+  (13, 'TechGuru Chatbot', 'A chatbot that answers tech-related questions and provides information about the latest gadgets and technology trends.', '2023-11-29', 7),
+  (14, 'ShopSaver', 'Helps users find the best deals, compare prices, and create shopping lists for a more budget-friendly shopping experience.', '2023-12-01', 2),
+  (15, 'FitFusion', 'Offers a variety of workout routines, tracks fitness progress, and provides nutritional tips to help users achieve their fitness goals.', '2024-02-20', 3),
+  (16, 'KidzCraft', 'An online space where children can explore and share their creative projects, including drawings, crafts, and stories.', '2023-03-03', 1);
 
 
 
